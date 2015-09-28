113fc1c09cb7529e1b016364d965b728363d713e
Andrew Esler <aj@esler.co.nz>
2015-09-28 20:07:33 +1300

    add photon-live-update.ino implementation

--- firmware/examples/photon-live-update.ino
+++ firmware/examples/photon-live-update.ino
@@ -1,1 +1,49 @@
+#include "application.h"
 #include "SmartMeterReader/SmartMeterReader.h"
+
+const int MIN_READING_GAP = 30; // upload a reading at most every 30 seconds.
+const int photoresistorPin = D3;
+const int pulseLedPin = D6;
+
+SmartMeterReader smartMeterReader(photoresistorPin);
+float reading;
+String stringReading;
+int lastUploadTime = 0;
+
+void recordFlashISR() {
+  smartMeterReader.recordFlash();
+}
+
+void uploadReading() {
+    int now = Time.now();
+    if (now - lastUploadTime > MIN_READING_GAP) {
+        Spark.publish("power-usage", stringReading);
+        lastUploadTime = now;
+    }
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(pulseLedPin, OUTPUT);
+  attachInterrupt(photoresistorPin, recordFlashISR, RISING);
+}
+
+void loop() {
+  if (smartMeterReader.hasReading()) {
+    reading = smartMeterReader.reading();
+
+    Serial.print("Latest reading is ");
+    Serial.print(reading, 2);
+    Serial.println(" kW");
+
+    // flash the led to indicate a meter flash has been recorded
+    digitalWrite(pulseLedPin, HIGH);
+    delay(10);
+    digitalWrite(pulseLedPin, LOW);
+
+    stringReading = String(reading, 2);
+    uploadReading();
+  } else {
+    delay(10);
+  }
+}
